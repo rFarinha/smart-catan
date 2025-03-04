d9d90659a030f327f88a42372956ef4686edec0d
rFarinha <Ricardoarfarinha@gmail.com>
2025-03-04 12:09:18 +0000

    fix: turn off all leds first before turnOnNumber so robber repeats animation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-// FIXME Change number buttons size with screen
-// FIXME repeating 7 should repeat animation
-// TODO Save all settings to Flash so when power is off
 // FIXME change all expansion names to extension
 // TODO HOME ASSISTANT
 // TODO CLEANING
@@ -436,6 +433,9 @@ void turnOnNumber()
   // Determine how many tiles to check (19 for classic, 30 for expansion)
   int tileCount = boardConfig.isExpansion ? 30 : 19;
   ledController.stopAnimation();
+
+  // turn off all leds
+  ledController.turnOffAllLeds();
 
   if (selectedNumber == 7)
   {
